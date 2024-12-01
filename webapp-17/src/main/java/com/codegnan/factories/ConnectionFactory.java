ac3fd7ebd2dc2ebe39014f7b772c888fe34e3813
ramasanisravani14 <134825613+ramasanisravani14@users.noreply.github.com>
2024-12-01 10:35:56 +0530

    Add files via upload

---
+++ webapp-17/src/main/java/com/codegnan/factories/ConnectionFactory.java
@@ -0,0 +1,22 @@
+package com.codegnan.factories;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+ 
+public class ConnectionFactory {
+          	private static Connection connection = null;
+          	static {
+                         	try {
+                                       	Class.forName("com.mysql.cj.jdbc.Driver");
+                                       	connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/adjava", "root", "root");
+                         	} catch (Exception e) {
+                                       	e.printStackTrace();
+                         	}
+          	}
+ 
+          	public static Connection getConnection() {
+                         	return connection;
+          	}
+ 
+}
+
